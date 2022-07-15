0a9309e41a55e7b8a353497d6afac8b84d9633a2
Gabriele N. Tornetta <phoenix1987@gmail.com>
2022-07-15 10:46:19 +0100

    refactor: change session measurement name format

--- f1_telemetry/webapp/utils.js
+++ f1_telemetry/webapp/utils.js
@@ -2,19 +2,9 @@ function toTimestamp(date) {
     return new Date(date).getTime()
 }
 
-function parseSessionId(sessionId) {
-    let dateIndex = (sessionId.indexOf("#") + 1) || (sessionId.indexOf(":") + 1);
-    let timeIndex = sessionId.indexOf("@") + 1;
-
-    if (timeIndex < dateIndex) {
-        dateIndex = 0;
-    }
-
-    let location = sessionId.substring(0, dateIndex - 1).toUpperCase() || "Unknown location";
-    let date = d3.timeFormat("%d %b %Y")(new Date(sessionId.substring(dateIndex, timeIndex - 1)));
-    let time = sessionId.substring(timeIndex);
-
-    return `<span class="location">${location}</span><br/><span class="datetime">${date} at ${time}</span>`;
+function parseSessionId(sessionId, laps) {
+    let [date, time, location] = sessionId.split('|');
+    return `<span class="location">${location} </span><span class="info">(${laps} lap${laps == 1 ? "" : "s"})</span><br/><span class="info">${date} at ${time}</span>`;
 }
 
 function arrayEquals(a, b) {
